03555b6551408a9c351fbd6c2a245d7353e580ce
Avej K Lanjekar <avej.lanjekar11@gmail.com>
2026-09-25 11:05:55 +0000

    fix hello.py indentation

--- backend/hello.py
+++ backend/hello.py
@@ -4,7 +4,7 @@ import mysql.connector
 
 
 class DBManager:
-   def __init__(
+    def __init__(
         self,
         database=os.getenv("DB_NAME", "example"),
         host=os.getenv("DB_HOST", "db"),
@@ -20,35 +20,48 @@ class DBManager:
             auth_plugin='mysql_native_password'
         )
 
-        self.cursor = self.connection.cursor() 
+        self.cursor = self.connection.cursor()
+
     def populate_db(self):
         self.cursor.execute('DROP TABLE IF EXISTS blog')
-        self.cursor.execute('CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))')
-        self.cursor.executemany('INSERT INTO blog (id, title) VALUES (%s, %s);', [(i, 'Blog post #%d'% i) for i in range (1,5)])
+        self.cursor.execute(
+            'CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))'
+        )
+        self.cursor.executemany(
+            'INSERT INTO blog (id, title) VALUES (%s, %s);',
+            [(i, 'Blog post #%d' % i) for i in range(1, 5)]
+        )
         self.connection.commit()
-    
+
     def query_titles(self):
         self.cursor.execute('SELECT title FROM blog')
         rec = []
+
         for c in self.cursor:
             rec.append(c[0])
+
         return rec
 
 
 server = Flask(__name__)
 conn = None
 
+
 @server.route('/')
 def listBlog():
     global conn
+
     if not conn:
         conn = DBManager()
         conn.populate_db()
+
     rec = conn.query_titles()
 
     response = ''
+
     for c in rec:
-        response = response  + '<div>   Hello  ' + c + '</div>'
+        response = response + '<div>   Hello  ' + c + '</div>'
+
     return response
 
 
